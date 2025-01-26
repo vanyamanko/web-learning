7d6466cd139a3464a44c3abbeeed8984219278e5
vanyamanko <ivanmanko04@icloud.com>
2025-01-26 23:03:16 +0300

    added log-in/sing-up

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
+
     gender = models.CharField(max_length=10, null=True, blank=True)
     email = models.EmailField(unique=True)
     country = models.CharField(max_length=100, null=True, blank=True)
